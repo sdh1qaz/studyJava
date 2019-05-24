0dd2c7da0c97b43016da36a4bb926febc2955995
苏登辉 <dhsu2@iflytek.com>
2019-05-25 03:36:29 +0800

    学习freemarker的使用

--- studyJava/src/学习json/freemarker/jdbc/FreemarkerTest.java
+++ studyJava/src/学习json/freemarker/jdbc/FreemarkerTest.java
@@ -1,7 +1,22 @@
 package 学习json.freemarker.jdbc;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
 import java.util.Map;
+
+import javax.print.DocFlavor.STRING;
+
 import com.dhsu.json.CsfJSonUtils;
+
+import freemarker.cache.StringTemplateLoader;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
 import org.junit.Test;
 
 /**
@@ -10,54 +25,55 @@ import org.junit.Test;
  * @Description: 学习freemarker的使用
  * @Date:Created in 2019年5月24日
  */
-class Student{
-	private String name;
-	private int age;
-	private Map<String, String> score;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public Map<String, String> getScore() {
-		return score;
-	}
-	public void setScore(Map<String, String> score) {
-		this.score = score;
-	}
-	@Override
-	public String toString() {
-		return "Student [name=" + name + ", age=" + age + ", score=" + score + "]";
-	}
-	
-}
+
 
 public class FreemarkerTest {
 	
 	@Test
-	public void testFreemarker() {
+	public void testFreemarker() throws IOException, TemplateException {
 		//json数据转化为字符串
 		String jStu = "{\r\n" + 
 				"    \"student\": {\r\n" + 
 				"        \"name\": \"张三\",\r\n" + 
-				"        \"age\": 20,\r\n" + 
+				"        \"age\": 29,\r\n" + 
 				"        \"score\": {\r\n" + 
 				"            \"语文\": \"90\",\r\n" + 
 				"            \"数学\": \"100\",\r\n" + 
-				"            \"英语\": \"80\"\r\n" + 
+				"            \"英语\": \"81\"\r\n" + 
 				"        }\r\n" + 
 				"    }\r\n" + 
 				"}"; 
 		//json串转化成bean对象
 		Student s = CsfJSonUtils.jsonString2Bean(jStu, "student", Student.class);
-		System.out.println(s.toString());
+		System.out.println("学生对象：" + s.toString());
 		
+		//从文件中读取模板
+		FileReader fReader = new FileReader("E:\\freemarker_model.txt");
+		BufferedReader bf = new BufferedReader(fReader);
+		String read;
+		StringBuilder sb = new StringBuilder();
+		while((read = bf.readLine()) != null) {
+			sb.append(read);
+		}
+		System.out.println("读取的模板：" + sb.toString());
+		//获取freemarker连接
+		Configuration cfg = new Configuration(Configuration.VERSION_2_3_23);
+		StringTemplateLoader stl = new StringTemplateLoader();
+		stl.putTemplate("mytemplate", sb.toString());
+		cfg.setTemplateLoader(stl);
+		
+		//放入数据
+		HashMap<String, Object> map = new HashMap<>();
+		//放入学生对象
+		map.put("s", s);
+		System.out.println("map的内容：" + map);
+		
+		Template template = cfg.getTemplate("mytemplate","UTF-8");
+		StringWriter sw = new StringWriter();
+		template.process(map, sw);
+		
+		//模板转换的最终内容
+		String cont = sw.toString();
+		System.out.println("模板填充后的内容：" + cont);
 	}
 }
---
+++ studyJava/src/学习json/freemarker/jdbc/Student.java
@@ -0,0 +1,39 @@
+package 学习json.freemarker.jdbc;
+
+import java.util.Map;
+
+/**
+ * @ClassName： Student
+ * @Author: dhSu
+ * @Description:
+ * @Date:Created in 2019年5月25日
+ */
+
+public class Student {
+	private String name;
+	private int age;
+	private Map<String, String> score;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Map<String, String> getScore() {
+		return score;
+	}
+	public void setScore(Map<String, String> score) {
+		this.score = score;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + ", score=" + score + "]";
+	}
+	
+}
