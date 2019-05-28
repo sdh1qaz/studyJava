8f884241485cd97a41cd027489847c58fcc40e90
苏登辉 <dhsu2@iflytek.com>
2019-05-28 17:40:38 +0800

    测试 for(;;)

---
+++ studyJava/src/for循环测试/ForLoopTest.java
@@ -0,0 +1,20 @@
+package for循环测试;
+
+import org.junit.Test;
+
+/**
+ * @ClassName： ForLoopTest
+ * @Author: dhSu
+ * @Description: 测试 for(;;)
+ * @Date:Created in 2019年5月28日
+ */
+
+public class ForLoopTest {
+	
+	@Test
+	public void testForLoop() {
+		for(;;) {
+			System.out.println("ss");
+		}
+	}
+}
