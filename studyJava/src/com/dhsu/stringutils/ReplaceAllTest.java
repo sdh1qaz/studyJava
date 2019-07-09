3d2b5a4bfda1f3e023566fef873b9a6839db3e7e
苏登辉 <dhsu2@iflytek.com>
2019-07-09 18:23:17 +0800

    学习使用String类的indexOf方法

--- studyJava/src/com/dhsu/stringutils/ReplaceAllTest.java
+++ studyJava/src/com/dhsu/stringutils/ReplaceAllTest.java
@@ -1,15 +1,27 @@
 package com.dhsu.stringutils;
+
+import org.junit.Test;
+
 /**
  * @ClassName： ReplaceAllTest
+ * 
  * @Author: dhSu
  * @Description:
  * @Date:Created in 2018年5月15日
  */
 
 public class ReplaceAllTest {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		String ttString = "你好，（支持流量结转）";
-		
+
 		System.out.println(ttString.replaceAll("（支持流量结转）", "%%%%%"));
+	}
+
+	@Test
+	public void test() {
+		String desc = "改用户订购了[大刘昂1234567890]";
+		String deString = desc.substring(0, desc.indexOf("]"));
+		desc = deString.replace("改用户订购了[", "");
+		System.out.println(desc);
 	}
 }
