35851268fe31f2338d69bebc1c9256ed3b2fc876
苏登辉 <dhsu2@iflytek.com>
2019-07-25 19:38:38 +0800

    freemarker遍历map

--- studyJava/src/学习json/freemarker/jdbc/FreemarkerTest.java
+++ studyJava/src/学习json/freemarker/jdbc/FreemarkerTest.java
@@ -76,4 +76,37 @@ public class FreemarkerTest {
 		String cont = sw.toString();
 		System.out.println("模板填充后的内容：" + cont);
 	}
+	
+	/**
+	 * freemarker遍历map
+	 */
+	@Test
+	public void testFreemarker1() throws IOException, TemplateException {
+		String template = "【已办业务查询】尊敬的客户，您好！您目前手机已开通业务有：\\r\\n一、主套餐：${prodPrcNameMain}。套餐资费详情：${prodPrcDescMain}\\r\\n二、<#if map?size gt 0>除主套餐外，您订购的业务及月功能费如下：<#list map?keys as key>${key}${map[\"${key}\"]}</#list>如需退订主套餐以外业务请发送77至10086按提示操作即可。<#else>除主套餐外，暂未查询到其他业务。您也可以发送免费短信77至10086按提示查询退订已开通业务。</#if>以上国内资费不包含港澳台。【中国移动】";
+		System.out.println("模板：" + template.toString());
+		//获取freemarker连接
+		Configuration cfg = new Configuration(Configuration.VERSION_2_3_23);
+		StringTemplateLoader stl = new StringTemplateLoader();
+		stl.putTemplate("mytemplate", template);
+		cfg.setTemplateLoader(stl);
+		
+		//放入数据
+		HashMap<String, Object> paramMap = new HashMap<>();
+		HashMap<String, Object> map = new HashMap<>();
+		//放入学生对象
+		/*map.put("套餐1", "套餐1每月100元");
+		map.put("套餐2", "套餐2每月100元");*/
+		paramMap.put("map", map);
+		paramMap.put("prodPrcNameMain", "飞享套餐188元");
+		paramMap.put("prodPrcDescMain", "该套餐免费");
+		System.out.println("数据内容：" + paramMap);
+		
+		Template tp = cfg.getTemplate("mytemplate","UTF-8");
+		StringWriter sw = new StringWriter();
+		tp.process(paramMap, sw);
+		
+		//模板转换的最终内容
+		String cont = sw.toString();
+		System.out.println("模板填充后的内容：" + cont);
+	}
 }
